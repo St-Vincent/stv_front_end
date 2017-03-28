8ff5bb105ca67546630fbdaa3f8e1580fa271bd7
St-Vincent <stvincenthealth@gmail.com>
2017-03-28 13:32:38 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -135,6 +135,23 @@ $(function () {
     });
 });
 
+// Service Line Locations CTA Smooth Scrolling ----------------------------------
+
+$(function() {
+   $('.location-cta a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 
 // Tooltip & Popovers ----------------------------------
 
